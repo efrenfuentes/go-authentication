79bc5c3b56012f120f19ab9944107b689a3dcdae
Efrén Fuentes <efrenfuentes@gmail.com>
2016-04-07 19:16:35 -0430

    Add method to get check user has an ability, add comments to exported methods and structs

--- models/user.go
+++ models/user.go
@@ -8,6 +8,7 @@ import (
 	"github.com/efrenfuentes/go-authentication/core/crypt"
 )
 
+// User keep user information like email and password for authentication
 type User struct {
 	ID        uint
 	CreatedAt *time.Time `json:"created_at"`
@@ -20,12 +21,14 @@ type User struct {
 	Groups []Group `gorm:"many2many:user_groups;"   json:"groups"`
 }
 
+// NewUser struct has the information to create a new user
 type NewUser struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+// AuthUser struct has the information to be use when an user try to authenticate
 type AuthUser struct {
 	Name         string `json:"name"`
 	Email        string `json:"email"`
@@ -33,10 +36,12 @@ type AuthUser struct {
 	ClientSecret string `json:"client_secret"`
 }
 
+// SetPassword sets the user password after encrypt it
 func (u *User) SetPassword(password string) {
 	u.EncryptPassword = crypt.Crypt(password, crypt.RandStringBytes(2))
 }
 
+// SetEmail sets the user email after validate it
 func (u *User) SetEmail(email string) error {
 	Re := regexp.MustCompile(`^\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3}$`)
 	isValid := Re.MatchString(email)
@@ -49,18 +54,34 @@ func (u *User) SetEmail(email string) error {
 	return errors.New("Email format is invalid " + email)
 }
 
+// Authenticate check if email and password are correct and if have
+// authorization for use the client
 func (u User) Authenticate(email, password, secretKey string) bool {
 	salt := u.EncryptPassword[0:2]
 
 	encrypted := crypt.Crypt(password, salt)
 
-	return (email == u.Email) && (encrypted == u.EncryptPassword) && u.HaveClientAccess(secretKey)
+	return (email == u.Email) && (encrypted == u.EncryptPassword) && u.HasClientAccess(secretKey)
 }
 
-func (u User) HaveClientAccess(secretKey string) bool {
+// HasClientAccess checks if user has authorization to authenticate in a client
+func (u User) HasClientAccess(secretKey string) bool {
 	for _, group := range u.Groups {
 		for _, client := range group.Clients {
 			if secretKey == client.ClientSecret {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// HasAbility checks if user has a specific ability
+func (u User) HasAbility(abilityName string) bool {
+	for _, group := range u.Groups {
+		for _, ability := range group.Abilities {
+			if (abilityName == ability.Name) || (ability.Name == "all") {
 				return true
 			}
 		}
